91319457a1d92e65cf86dbd05b774e1512a36c08
Joshua Harms <joshua@nozzlegear.com>
2025-09-23 12:58:37 -0500

    Rewrite lib/main.dart for Dart 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,214 +1,243 @@
-library sci_tally_tool;
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
-import "dart:async";
-import "dart:io" show Platform;
-import "dart:convert" show UTF8, BASE64, Encoding, JSON;
-import "package:logging/logging.dart";
-import "package:intl/intl.dart" show DateFormat;
 import 'package:http/http.dart' as http;
-import "package:dson/dson.dart";
+import 'package:intl/intl.dart';
+import 'package:logging/logging.dart';
 
-part "main.g.dart";
+final log = Logger('main');
 
-final Logger log = new Logger("main");
-
-String envVarRequired(String key) {
-  if (Platform.environment.containsKey(key)) {
-    return Platform.environment[key];
+String envReq(String key) {
+  final v = Platform.environment[key];
+  if (v == null || v.isEmpty) {
+    throw StateError('Missing env: $key');
   }
-
-  throw "Environment variable \"${key}\" was empty.";
+  return v;
 }
 
-String envVarDefault(String key, String defaultValue) {
-  if (Platform.environment.containsKey(key)) {
-    return Platform.environment[key];
-  }
-
-  return defaultValue;
-}
+String envOr(String key, String def) => Platform.environment[key] ?? def;
 
 bool envIsLive() {
-  String env = envVarDefault("SCI_TALLY_ENV", "development");
-  String qs = envVarDefault("Http_Query", "");
-
-  return env == "production" || qs.contains("env=production");
+  final env = envOr('SCI_TALLY_ENV', 'development');
+  final qs = envOr('Http_Query', '');
+  return env == 'production' || qs.contains('env=production');
 }
 
-DateTime getMidnight() {
-  DateTime now = new DateTime.now();
-
-  return new DateTime(now.year, now.month, now.day);
+DateTime midnightLocal() {
+  final now = DateTime.now();
+  return DateTime(now.year, now.month, now.day);
 }
 
-String buildApiUrl(DateTime startDate, DateTime endDate) {
-  String apiDomain = envVarRequired("SCI_TALLY_API_DOMAIN");
-  String protocol = apiDomain.contains("localhost") ? "http" : "https";
-
-  return "$protocol://$apiDomain/api/v1/orders/tally/sources?since=${startDate.millisecondsSinceEpoch}&until=${endDate.millisecondsSinceEpoch}";
+Uri buildApiUrl(DateTime startDate, DateTime endDate) {
+  final apiDomain = envReq('SCI_TALLY_API_DOMAIN');
+  final protocol = apiDomain.contains('localhost') ? 'http' : 'https';
+  return Uri.parse(
+    '$protocol://$apiDomain/api/v1/orders/tally/sources'
+    '?since=${startDate.millisecondsSinceEpoch}'
+    '&until=${endDate.millisecondsSinceEpoch}',
+  );
 }
 
-Future<http.Response> makeRequest(Uri url, String method,
-    {String body = null, Map<String, String> customHeaders = null}) async {
-  customHeaders ??= {};
+Future<http.Response> _req(
+  Uri url,
+  String method, {
+  String? body,
+  Map<String, String>? customHeaders,
+}) async {
+  final headers = <String, String>{
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'User-Agent': 'sci_tally_tool/2 (com.nozzlegear.sci_tally_tool)',
+    ...?customHeaders,
+  };
 
-  Map<String, String> headers = new Map.from(customHeaders)
-    ..putIfAbsent("Content-Type", () => "application/json")
-    ..putIfAbsent("Accept", () => "application/json");
-  http.Response resp;
-
-  switch (method.toLowerCase()) {
-    case "post":
-      resp = await http.post(url, body: body, encoding: Encoding.getByName("UTF8"), headers: headers);
-      break;
-    case "get":
-      resp = await http.get(url, headers: headers);
-      break;
+  switch (method.toUpperCase()) {
+    case 'POST':
+      return http.post(url, body: body, headers: headers).timeout(const Duration(seconds: 25));
+    case 'GET':
+      return http.get(url, headers: headers).timeout(const Duration(seconds: 25));
     default:
-      throw new UnimplementedError("Attempted to create HttpClient with unsupported HTTP method $method.");
+      throw UnimplementedError('Unsupported method $method');
   }
-
-  return resp;
 }
 
-void ensureSuccessResponse(http.Response resp) {
+void _ensureSuccess(http.Response resp) {
   if (resp.statusCode < 200 || resp.statusCode >= 300) {
-    String message = "Request to ${resp.request.url} failed with ${resp.statusCode} ${resp.reasonPhrase}.";
-
-    log.severe("$message Response body: ${resp.body}");
-
-    throw new StateError(message);
+    final message =
+        'Request to ${resp.request?.url} failed with ${resp.statusCode} ${resp.reasonPhrase}.';
+    log.severe('$message Response body: ${resp.body}');
+    throw StateError(message);
   }
 }
 
-Future<T> makeGetRequest<T>(String url, [Map<String, String> headers = null]) async {
-  var resp = await makeRequest(Uri.parse(url), "GET", customHeaders: headers);
-
-  ensureSuccessResponse(resp);
-
-  return JSON.decode(resp.body);
+Future<String> getBody(String url, [Map<String, String>? headers]) async {
+  final r = await _req(Uri.parse(url), 'GET', customHeaders: headers);
+  _ensureSuccess(r);
+  return r.body;
 }
 
-Future<T> makePostRequest<T>(String url, String body, [Map<String, String> headers = null]) async {
-  http.Response resp = await makeRequest(Uri.parse(url), "POST", body: body, customHeaders: headers);
-
-  ensureSuccessResponse(resp);
-
-  return JSON.decode(resp.body);
+Future<String> postBody(String url, String body,
+    [Map<String, String>? headers]) async {
+  final r =
+      await _req(Uri.parse(url), 'POST', body: body, customHeaders: headers);
+  _ensureSuccess(r);
+  return r.body;
 }
 
 SwuMessage buildEmailData(DateTime startDate, DateTime endDate, List<TallyTemplate> tally) {
   final isLive = envIsLive();
-  final swuTemplateId = envVarRequired("SCI_TALLY_SWU_TEMPLATE_ID");
-  final SwuRecipient emailRecipient = fromJson(envVarRequired("SCI_TALLY_PRIMARY_RECIPIENT"), SwuRecipient);
-  final List<SwuRecipient> ccs = isLive ? fromJson(envVarRequired("SCI_TALLY_CC_LIST"), [List, SwuRecipient]) : [];
-  final SwuSender sender = fromJson(envVarRequired("SCI_TALLY_SENDER"), SwuSender);
+  final swuTemplateId = envReq('SCI_TALLY_SWU_TEMPLATE_ID');
+  final emailRecipient =
+      SwuRecipient.fromJson(jsonDecode(envReq('SCI_TALLY_PRIMARY_RECIPIENT')));
+  final ccs = isLive
+      ? (jsonDecode(envReq('SCI_TALLY_CC_LIST')) as List)
+          .map((e) => SwuRecipient.fromJson(e as Map<String, dynamic>))
+          .toList()
+      : <SwuRecipient>[];
+  final sender =
+      SwuSender.fromJson(jsonDecode(envReq('SCI_TALLY_SENDER')));
 
-  return new SwuMessage()
-    ..template = swuTemplateId
-    ..recipient = emailRecipient
-    ..cc = ccs
-    ..sender = sender
-    ..template_data = (new SwuTallyTemplateData()
-      ..startDate = new DateFormat("MMM dd, yyyy").format(startDate)
-      ..endDate = new DateFormat("MMM dd, yyyy").format(endDate)
-      ..tally = tally);
+  return SwuMessage(
+    template: swuTemplateId,
+    recipient: emailRecipient,
+    cc: ccs,
+    sender: sender,
+    templateData: SwuTallyTemplateData(
+      startDate: DateFormat('MMM dd, yyyy').format(startDate),
+      endDate: DateFormat('MMM dd, yyyy').format(endDate),
+      tally: tally,
+    ),
+  );
 }
 
-String encode(Object data) {
-  dynamic toEncodable(arg) {
-    if (arg is Iterable) {
-      // JSON codec doesn't know how to encode iterable, but it does know how to encode List.
-      // Still doesn't answer the question of why a List<Map> registers as Iterable rather than
-      // a List in the first place.
-      return arg.toList();
-    }
-
-    // This will throw an exception if the object doesn't implement toJson. An exception will be thrown
-    // anyway as the codec won't know how to encode it.
-    return arg.toJson();
-  }
-
-  return JSON.encode(data, toEncodable: toEncodable);
-}
-
-Future main(List<String> args) async {
-  _initMirrors();
+Future<void> main(List<String> args) async {
   Logger.root.level = Level.ALL;
-  Logger.root.onRecord.listen((LogRecord rec) {
-    print('[${rec.level.name}] ${rec.time}: ${rec.message}');
+  Logger.root.onRecord.listen((rec) {
+    stdout.writeln('[${rec.level.name}] ${rec.time}: ${rec.message}');
   });
 
-  log.info("SCI Tally Tool starting up.");
+  log.info('SCI Tally Tool starting up.');
 
-  final endDate = getMidnight();
-  final startDate = new DateTime(endDate.year, endDate.month, endDate.day - 7);
-  final url = buildApiUrl(startDate, endDate);
+  final endDate = midnightLocal();
+  final startDate = DateTime(endDate.year, endDate.month, endDate.day - 7);
+  final apiUrl = buildApiUrl(startDate, endDate);
 
-  log.info("Getting tally from ${url}.");
-
-  Iterable<TallyTemplate> tally = await makeGetRequest<Map<String, int>>(url)
-      .then((map) => map.keys.map((key) => new TallyTemplate(key, map[key])));
-  SwuMessage emailMessage = buildEmailData(startDate, endDate, tally);
-  final swuKey = envVarRequired("SCI_TALLY_SWU_KEY");
-  final headers = {
-    "Authorization": "Basic ${BASE64.encode(UTF8.encode("$swuKey:"))}",
+  final swuKey = envReq('SCI_TALLY_SWU_KEY');
+  final auth = base64Encode(utf8.encode('$swuKey:'));
+  final swuHeaders = {
+    'Authorization': 'Basic $auth',
   };
 
-  log.info("Sending to: ${emailMessage.recipient.address}");
+  log.info('Getting tally from $apiUrl.');
 
-  for (var cc in emailMessage.cc) {
-    log.info("CCed to: ${cc.address}");
+  final body = await getBody(apiUrl.toString());
+  final tallyData = jsonDecode(body) as Map<String, dynamic>;
+
+  final tally = [
+    for (final e in tallyData.entries)
+      TallyTemplate(source: e.key, count: (e.value as num).toInt())
+  ];
+
+  final message = buildEmailData(startDate, endDate, tally);
+
+  log.info('Sending to: ${message.recipient.address}');
+  for (final cc in message.cc) {
+    log.info('CCed to: ${cc.address}');
   }
 
-  Map<String, Object> emailResult =
-      await makePostRequest("https://api.sendwithus.com/api/v1/send", encode(emailMessage), headers);
+  final result = await postBody(
+    'https://api.sendwithus.com/api/v1/send',
+    jsonEncode(message),
+    swuHeaders,
+  );
 
-  log.info("Send result: $emailResult");
+  log.info('Send result: $result');
 }
 
-@serializable
-class TallyTemplate extends _$TallyTemplateSerializable {
-  String source;
-  int count;
+class TallyTemplate {
+  final String source;
+  final int count;
 
-  TallyTemplate(this.source, this.count);
+  TallyTemplate({required this.source, required this.count});
+
+  Map<String, dynamic> toJson() => {
+        'source': source,
+        'count': count,
+      };
 }
 
-@serializable
-class SwuRecipient extends _$SwuRecipientSerializable {
+class SwuRecipient {
   final String name;
   final String address;
 
-  // SwuRecipient();
-  SwuRecipient(this.name, this.address);
+  SwuRecipient({required this.name, required this.address});
+
+  factory SwuRecipient.fromJson(Map<String, dynamic> j) => SwuRecipient(
+        name: j['name'] as String,
+        address: j['address'] as String,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'address': address,
+      };
 }
 
-@serializable
-class SwuSender extends _$SwuSenderSerializable {
-  String name;
-  String address;
-  String replyTo;
+class SwuSender {
+  final String name;
+  final String address;
+  final String replyTo;
 
-  SwuSender(this.name, this.address, this.replyTo);
+  SwuSender({required this.name, required this.address, required this.replyTo});
+
+  factory SwuSender.fromJson(Map<String, dynamic> j) => SwuSender(
+        name: j['name'] as String,
+        address: j['address'] as String,
+        replyTo: j['replyTo'] as String,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'address': address,
+        'replyTo': replyTo,
+      };
 }
 
-@serializable
-class SwuTallyTemplateData extends _$SwuTallyTemplateDataSerializable {
-  String startDate;
-  String endDate;
-  List<TallyTemplate> tally;
+class SwuTallyTemplateData {
+  final String startDate;
+  final String endDate;
+  final List<TallyTemplate> tally;
 
-  SwuTallyTemplateData();
+  SwuTallyTemplateData({required this.startDate, required this.endDate, required this.tally});
+
+  Map<String, dynamic> toJson() => {
+        'startDate': startDate,
+        'endDate': endDate,
+        'tally': tally.map((t) => t.toJson()).toList(),
+      };
 }
 
-@serializable
-class SwuMessage extends _$SwuMessageSerializable {
-  String template;
-  SwuRecipient recipient;
-  List<SwuRecipient> cc;
-  SwuSender sender;
-  SwuTallyTemplateData template_data;
+class SwuMessage {
+  final String template;
+  final SwuRecipient recipient;
+  final List<SwuRecipient> cc;
+  final SwuSender sender;
+  final SwuTallyTemplateData templateData;
 
-  SwuMessage();
+  SwuMessage({
+    required this.template,
+    required this.recipient,
+    required this.cc,
+    required this.sender,
+    required this.templateData,
+  });
+
+  Map<String, dynamic> toJson() => {
+    'template': template,
+    'recipient': recipient.toJson(),
+    'cc': cc.map((r) => r.toJson()).toList(),
+    'sender': sender.toJson(),
+    'template_data': templateData.toJson(),
+  };
 }
